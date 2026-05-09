920aecac1e64b328bef99daf988a837edbf8ca34
Francisco <fdhm3011@gmail.com>
2026-05-08 23:54:31 -0400

    docs
    implementado swagger en api-gateway con soporte para auth jwt

--- apps/api-gateway/src/app/app.service.ts
+++ apps/api-gateway/src/app/app.service.ts
@@ -1,10 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { JwtService } from '@nestjs/jwt';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly httpService: HttpService) { }
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly jwtService: JwtService // Inyectamos el servicio de JWT
+  ) { }
+
+  async login(usuario: string, clave: string) {
+    // Simulamos un usuario de la base de datos del Grupo Cordillera
+    if (usuario === 'admin@cordillera.com' && clave === '123456') {
+      const payload = { email: usuario, role: 'admin' };
+
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+        mensaje: 'Bienvenido al Sistema de Gestión Cordillera'
+      };
+    }
+
+    throw new UnauthorizedException('Credenciales incorrectas');
+  }
 
   // Esta función es el "puente" hacia tu microservicio de KPIs
   async obtenerKpisDesdeMicroservicio() {
