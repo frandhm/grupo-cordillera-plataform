0f93cb0faba8e35f33107a46ca54d720cc719dfd
Francisco <fdhm3011@gmail.com>
2026-05-08 23:42:33 -0400

    feat
    implementado JWT AuthGuard para proteger rutas del dashboard

--- apps/api-gateway/src/app/app.controller.ts
+++ apps/api-gateway/src/app/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth.guard'; // Importamos el guardia
 
-@Controller('dashboard') // Cambiamos el nombre para que sea más claro
+@Controller('dashboard')
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @UseGuards(AuthGuard) // <--- ¡AQUÍ ESTÁ EL CANDADO!
   @Get('kpis')
   async getKpis() {
-    // Cuando alguien llame a localhost:3000/api/dashboard/kpis
-    // el Gateway irá a buscar los datos al microservicio (3001)
     return await this.appService.obtenerKpisDesdeMicroservicio();
   }
 }
--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    HttpModule, // Importante: Esto permite que el Gateway actúe como cliente
+    HttpModule,
+    JwtModule.register({
+      global: true,
+      secret: 'CLAVE_SECRETA_CORDILLERA', // La misma clave que en el Guardia
+      signOptions: { expiresIn: '1h' },    // El token dura 1 hora
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
