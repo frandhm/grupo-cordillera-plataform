685ec777f9630ecdefcf5c35c8a043ea4d395ac7
ninikyu <sca.perezm@duocuc.cl>
2026-05-08 23:11:11 -0400

    feat(ms-metas): se implementa MetaController con endpoints GET POST PUT DELETE

--- apps/ms-metas/src/app/app.controller.ts
+++ apps/ms-metas/src/app/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('metas')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly metaService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  async obtenerMetas() {
+    return await this.metaService.obtenerTodas();
+  }
+
+  @Get(':id')
+  async obtenerMetaPorId(@Param('id') id: string) {
+    return await this.metaService.obtenerPorId(id);
+  }
+
+  @Post()
+  async crearMeta(@Body() datos: any) {
+    return await this.metaService.crearMeta(datos);
+  }
+
+  @Put(':id')
+  async actualizarMeta(@Param('id') id: string, @Body() datos: any) {
+    return await this.metaService.actualizarMeta(id, datos);
+  }
+
+  @Delete(':id')
+  async eliminarMeta(@Param('id') id: string) {
+    return await this.metaService.eliminarMeta(id);
   }
 }
