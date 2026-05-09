1152f69c7754e81e004c69f6104c37b4e19edd7f
ninikyu <sca.perezm@duocuc.cl>
2026-05-08 23:11:09 -0400

    feat(ms-metas): se configura conexion TypeORM con PostgreSQL

--- apps/ms-metas/src/app/app.module.ts
+++ apps/ms-metas/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetaEntity } from './meta.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'user_cordillera',
+      password: 'cordillera0101',
+      database: 'metas_db',
+      entities: [MetaEntity],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([MetaEntity]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
